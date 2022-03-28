f061de2cae8457d3ca266b6276374c2c2b418a0d
Gordon Hayes <pggordonhayes@gmail.com>
2022-03-28 09:55:58 +0200

    02: setup riverpod

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,22 +1,18 @@
 export 'utils.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class GoogleDocsApp extends StatelessWidget {
+class GoogleDocsApp extends ConsumerStatefulWidget {
   const GoogleDocsApp({Key? key}) : super(key: key);
 
   @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _GoogleDocsAppState();
+}
+
+class _GoogleDocsAppState extends ConsumerState<GoogleDocsApp> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
-    );
+    return Container();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_docs_clone/app/app.dart';
 
 void main() {
   setupLogger();
 
-  runApp(const GoogleDocsApp());
+  runApp(const ProviderScope(child: GoogleDocsApp()));
 }
