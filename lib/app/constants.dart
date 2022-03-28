4a88a22abe0dbca96be30f29bcf1746dde41e42f
Gordon Hayes <pggordonhayes@gmail.com>
2022-03-28 12:04:08 +0200

    04: appwrite setup

---
+++ lib/app/constants.dart
@@ -0,0 +1,2 @@
+const appwriteEndpoint = 'http://localhost/v1';
+const appwriteProjectId = '6241851fa5fe35076532'; // TODO: modify this
---
+++ lib/app/providers.dart
@@ -0,0 +1,27 @@
+import 'package:appwrite/appwrite.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_docs_clone/app/constants.dart';
+
+abstract class Dependency {
+  static Provider<Client> get client => _clientProvider;
+  static Provider<Database> get database => _databaseProvider;
+  static Provider<Account> get account => _accountProvider;
+  static Provider<Realtime> get realtime => _realtimeProvider;
+}
+
+final _clientProvider = Provider<Client>(
+  (ref) => Client()
+    ..setProject(appwriteProjectId)
+    ..setSelfSigned(status: true)
+    ..setEndpoint(appwriteEndpoint),
+);
+
+final _databaseProvider =
+    Provider<Database>((ref) => Database(ref.read(_clientProvider)));
+
+final _accountProvider = Provider<Account>(
+  (ref) => Account(ref.read(_clientProvider)),
+);
+
+final _realtimeProvider =
+    Provider<Realtime>((ref) => Realtime(ref.read(_clientProvider)));
